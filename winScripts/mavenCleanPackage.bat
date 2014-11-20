efe6533196b069e8822be07f33637e7f01bd6512
dinesharun <dnsh.arun@gmail.com>
2014-11-20 16:43:01 +0100

    Update gitignore

---
+++ winScripts/mavenCleanPackage.bat
@@ -0,0 +1,5 @@
+cd ..
+SET JAVA_HOME=C:\Documents and Settings\extern.kasipandian\My Documents\Dinesh\Programs\jdk7
+SET PATH=C:\WINDOWS\system32;C:\Documents and Settings\extern.kasipandian\My Documents\Dinesh\Programs\jdk7\bin;C:\Documents and Settings\extern.kasipandian\My Documents\Dinesh\Programs\apache-maven-3.2.3\bin
+call mvn clean package
+pause
